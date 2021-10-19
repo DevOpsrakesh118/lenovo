9c228634fdb540606dbb70a50961b4977c0eefeb
uppununthalarakesh <rakeshuppununthala@gmail.com>
2021-10-19 11:53:57 +0530

    commint

--- DBUtils.py
+++ DBUtils.py
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 this is updated in master branch
 this line is updated in stage branch
+=======
+this line is updated in master branch
+>>>>>>> parent of 6f22174 (Revert "commit in master")
 iam updating this line...
 this file contains some python code 
